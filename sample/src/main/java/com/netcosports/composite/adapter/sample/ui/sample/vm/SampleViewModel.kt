315ec1115405f0d20093afb6291ac3a238d24388
Woffkaa <Vladimir.garkovich@netcosports.com>
2021-07-05 14:32:50 +0300

    feat: update sample

--- sample/src/main/java/com/netcosports/composite/adapter/sample/ui/sample/vm/SampleViewModel.kt
+++ sample/src/main/java/com/netcosports/composite/adapter/sample/ui/sample/vm/SampleViewModel.kt
@@ -1,83 +1,44 @@
 package com.netcosports.composite.adapter.sample.ui.sample.vm
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
-import android.util.TypedValue
-import android.view.View
+import android.app.Application
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.recyclerview.widget.RecyclerView
 import com.netcosports.composite.adapter.cell.Cell
-import com.netcosports.composite.adapter.cell.ClickItem
-import com.netcosports.composite.adapter.decoration.SpaceItemDecoration
 import com.netcosports.composite.adapter.sample.domain.entity.MessageEntity
 import com.netcosports.composite.adapter.sample.ui.di.SampleDI
-import com.netcosports.composite.adapter.sample.ui.sample.cell.common.SampleErrorCell
-import com.netcosports.composite.adapter.sample.ui.sample.cell.common.SampleFullScreenErrorCell
-import com.netcosports.composite.adapter.sample.ui.sample.cell.common.SampleFullScreenLoaderCell
-import com.netcosports.composite.adapter.sample.ui.sample.cell.common.SampleLoaderCell
-import com.netcosports.composite.adapter.sample.ui.sample.cell.databinding.SampleDataBindingMessageCell
-import com.netcosports.composite.adapter.sample.ui.sample.cell.view.SampleViewMessageCell
-import com.netcosports.composite.adapter.sample.ui.sample.cell.viewbinding.SampleViewBindingMessageCell
+import com.netcosports.composite.adapter.sample.ui.sample.SampleMapper
 import kotlinx.coroutines.*
 import java.util.*
 
 class SampleViewModel : ViewModel() {
 
     val state: SampleState = SampleState()
+
+    private val app: Application = SampleDI.provideApp()
+    private var toast: Toast? = null
+    private val mapper = SampleMapper(
+        app = app,
+        itemsPerPage = ITEMS_PER_PAGE,
+        viewMessageClickListener = {
+            toast?.cancel()
+            toast = Toast.makeText(app, "viewMessageClickListener", Toast.LENGTH_SHORT).also { it.show() }
+        },
+        viewBindingMessageClickListener = { click ->
+            changeMessageType(click.item)
+        },
+        dataBindingMessageClickListener = { click ->
+            changeMessageType(click.item)
+        }
+    )
+
     private var nextPage: Int = FIRST_PAGE
     private var isPaginationByScrollAllowed = true
 
-    private val firstItemDecoration: SpaceItemDecoration<Cell<*>>
-    private val middleSmallItemDecoration: SpaceItemDecoration<Cell<*>>
-    private val middleBigItemDecoration: SpaceItemDecoration<Cell<*>>
-    private val lastItemDecoration: SpaceItemDecoration<Cell<*>>
-
-    private var toast: Toast? = null
-    private val viewMessageClickListener: (ClickItem<MessageEntity>) -> Unit = {
-        toast?.cancel()
-        toast = Toast.makeText(SampleDI.provideApp(), "viewMessageClickListener", Toast.LENGTH_SHORT).also { it.show() }
-    }
-    private val viewBindingMessageClickListener: (ClickItem<MessageEntity>) -> Unit = {
-        changeMessageType(it.item)
-    }
-    private val dataBindingMessageClickListener: (ClickItem<MessageEntity>) -> Unit = {
-        changeMessageType(it.item)
-    }
     private var loadJob: Job? = null
     private var clickJob: Job? = null
 
     init {
-        val app = SampleDI.provideApp()
-        val space = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10f, app.resources.displayMetrics).toInt()
-        firstItemDecoration = SpaceItemDecoration(top = 8 * space, bottom = space, start = 8 * space, end = 8 * space)
-        middleSmallItemDecoration = object : SpaceItemDecoration<Cell<*>>(
-            top = 2 * space, bottom = 2 * space, start = 4 * space, end = 4 * space
-        ) {
-            private val itemBounds = Rect()
-            private val dividerPaint: Paint = Paint().apply {
-                this.color = Color.GREEN
-                this.style = Paint.Style.FILL
-                this.isAntiAlias = true
-            }
-
-            override fun onDraw(canvas: Canvas, view: View, parent: RecyclerView, state: RecyclerView.State, item: Cell<*>) {
-                super.onDraw(canvas, view, parent, state, item)
-                parent.layoutManager?.getDecoratedBoundsWithMargins(view, itemBounds)
-                canvas.drawRect(
-                    itemBounds.left.toFloat(),
-                    itemBounds.top.toFloat(),
-                    itemBounds.right.toFloat(),
-                    itemBounds.bottom.toFloat(),
-                    dividerPaint
-                )
-            }
-        }
-        middleBigItemDecoration = SpaceItemDecoration(top = space, bottom = space, start = space, end = space)
-        lastItemDecoration = SpaceItemDecoration(top = space, bottom = 8 * space, start = 8 * space, end = 8 * space)
         loadData(nextPage)
     }
 
@@ -112,7 +73,10 @@ class SampleViewModel : ViewModel() {
                 } else {
                     state.messages
                 }
-                cellsOnLoading = mapToCells(messagesOnLoading, getLoaderCell(fullscreen = messagesOnLoading.isEmpty()))
+                cellsOnLoading = mapper.mapToCells(
+                    messages = messagesOnLoading,
+                    extraCell = mapper.getLoaderCell(fullscreen = messagesOnLoading.isEmpty())
+                )
             }
             state.setData(messagesOnLoading, cellsOnLoading)
 
@@ -140,13 +104,13 @@ class SampleViewModel : ViewModel() {
                     } else {
                         state.messages + messages
                     }
-                    newCells = mapToCells(newMessages)
+                    newCells = mapper.mapToCells(newMessages)
                 } else {
                     newMessages = state.messages
-                    newCells = mapToCells(
-                        newMessages,
-                        getErrorCell(
-                            getErrorMessage(random),
+                    newCells = mapper.mapToCells(
+                        messages = newMessages,
+                        extraCell = mapper.getErrorCell(
+                            mapper.getErrorMessage(random),
                             fullscreen = newMessages.isEmpty(),
                             onRetryClickListener = { loadData(page) }
                         )
@@ -161,89 +125,13 @@ class SampleViewModel : ViewModel() {
         clickJob?.cancel()
         clickJob = viewModelScope.launch {
             val newMessages = withContext(Dispatchers.IO) {
-                state.messages.map { message ->
-                    if (input.id == message.id) {
-                        message.copy(type = MessageEntity.Type.values().toList().minus(input.type).random())
-                    } else {
-                        message
-                    }
-                }
+                mapper.changeMessageType(state.messages, input)
             }
-            state.setData(newMessages, mapToCells(newMessages))
+            state.setData(newMessages, mapper.mapToCells(newMessages))
         }
-    }
-
-    private fun mapToCells(messages: List<MessageEntity>, extraCell: Cell<*>? = null): List<Cell<*>> {
-        val result = mutableListOf<Cell<*>>()
-
-        messages.mapIndexedTo(result) { i, message ->
-            val decoration = when {
-                i == 0 -> firstItemDecoration
-                i == messages.size - 1 -> lastItemDecoration
-                else -> {
-                    if (i % ((ITEMS_PER_PAGE + messages.size) / ITEMS_PER_PAGE) == 0) {
-                        middleSmallItemDecoration
-                    } else {
-                        middleBigItemDecoration
-                    }
-                }
-            }
-            when (message.type) {
-                MessageEntity.Type.VIEW -> SampleViewMessageCell(
-                    message.copy(text = "ViewMessageCell ${message.text}"),
-                    decoration = decoration,
-                    onClickListener = viewMessageClickListener
-                )
-                MessageEntity.Type.VIEW_BINDING -> SampleViewBindingMessageCell(
-                    message.copy(text = "ViewBindingMessageCell ${message.text}"),
-                    decoration = decoration,
-                    onClickListener = viewBindingMessageClickListener
-                )
-                MessageEntity.Type.DATA_BINDING -> SampleDataBindingMessageCell(
-                    message.copy(text = "DataBindingMessageCell ${message.text}"),
-                    decoration = decoration,
-                    onClickListener = dataBindingMessageClickListener
-                )
-            }
-        }
-        if (extraCell != null) {
-            result.add(extraCell)
-        }
-
-        return result
-    }
-
-    private fun getErrorMessage(random: Random): String {
-        val diff = 'z' - 'a'
-        val errorMessage = (0..5).map {
-            ('a' + random.nextInt(diff)).let { char ->
-                if (random.nextBoolean()) {
-                    char
-                } else {
-                    char.uppercase()
-                }
-            }
-        }.joinToString("")
-        return "Error $errorMessage"
     }
 
     private fun getNextPage(currentPage: Int): Int = currentPage + 1
-
-    private fun getLoaderCell(fullscreen: Boolean): Cell<*> {
-        return if (fullscreen) {
-            SampleFullScreenLoaderCell()
-        } else {
-            SampleLoaderCell()
-        }
-    }
-
-    private fun getErrorCell(error: String, fullscreen: Boolean, onRetryClickListener: () -> Unit): Cell<*> {
-        return if (fullscreen) {
-            SampleFullScreenErrorCell(data = error, onRetryClickListener = onRetryClickListener)
-        } else {
-            SampleErrorCell(data = error, onRetryClickListener = onRetryClickListener)
-        }
-    }
 
     override fun onCleared() {
         super.onCleared()
